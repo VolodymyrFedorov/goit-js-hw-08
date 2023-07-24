da9bcf525e031e73157c893897d4b074ade20e39
VolodymyrFedorov <133118295+VolodymyrFedorov@users.noreply.github.com>
2023-07-24 17:04:20 +0300

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,31 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from "../../node_modules/simplelightbox";
+import "../../node_modules/";
+
+const gallery = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems
+  .map(item => `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${item.original}">
+        <img
+          class="gallery__image"
+          src="${item.preview}"
+          data-source="${item.original}"
+          alt="${item.description}"
+        />
+      </a>
+    </li>
+  `)
+  .join('');
+
+gallery.innerHTML = galleryMarkup;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt', 
+  captionDelay: 250,
+});
+
 console.log(galleryItems);
