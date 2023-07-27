1a6b518307fec50a4e95be24e839d0e08b6e4004
VolodymyrFedorov <133118295+VolodymyrFedorov@users.noreply.github.com>
2023-07-27 20:49:42 +0300

    fixes

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,31 +1,40 @@
 import throttle from 'lodash.throttle';
-import { loadFromLS, saveToLS } from './helpers.js';
+import { loadFromLS, saveToLS } from './helpers.js'; 
 
 const refs = {
   form: document.querySelector('.feedback-form'), 
 };
 
-refs.form.addEventListener('input', throttle(e => { 
-  const key = e.target.name;    
-  const value = e.target.value; 
-  saveToLS(key, value);         
+const key = 'feedback-form-state';
+let userData = {
+    email: refs.form.elements.email.value,
+    message: refs.form.elements.message.value
+  }
+
+refs.form.addEventListener('input', throttle(() => { 
+   userData = {
+    email: refs.form.elements.email.value,
+    message: refs.form.elements.message.value,
+  };
+  saveToLS(key, userData);         
 },500));
 
 refs.form.addEventListener('submit', e => {    
-  e.preventDefault();        
+  e.preventDefault();   
   const email = e.target.elements.email.value; 
   const message = e.target.elements.message.value; 
   console.log({ email, message }); 
   localStorage.clear(); 
-  e.target.reset();     
+  e.target.reset();   
 });
 
 function onPageLoad() {       
-  const email = loadFromLS('email'); 
-  const message = loadFromLS('message');
-
-  refs.form.elements.email.value = email || '';
-  refs.form.elements.message.value = message || '';
+  const dataFromLS = loadFromLS(key);
+  if (dataFromLS) {
+    userData = dataFromLS;
+  };
+  refs.form.elements.email.value = userData.email || ''; 
+  refs.form.elements.message.value = userData.message || ''; 
 }
 onPageLoad(); 
-   
+  
